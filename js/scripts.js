57e58c08ed12ecf62362026c724cd12469a5a4ee
CameronWill <cellardoorsolar@aol.com>
2020-08-24 13:13:53 -0700

    fill-in readme specs
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Destinations() {
 } 
 
 Destinations.prototype.addCity = function(city) {
-  cities.id = this.assignId();
+  city.id = this.assignId();
   this.cities.push(city);
 }
 
@@ -16,27 +16,43 @@ Destinations.prototype.assignId = function() {
 
 Destinations.prototype.findCity = function(id) {
   for (let i = 0; i < this.cities.length; i++) {
-    if (cities[i]) {
+      // if (cities[i]) {
       if (this.cities[i].id == id) {
         return this.cities[i];
       }
-    }
+    //  }
   };
   return false;
 }
 
-// business logic  worldCities
-function WorldCities(landmark, timeOfYeArray, tagline) {
+// business logic worldCities
+function WorldCities(landmark, timeOfYear, tagline) {
   this.landmark = landmark;
-  this.timeOfYeArray = timeOfYeArray;
+  this.timeOfYear = timeOfYear;
   this.tagline = tagline;
 }
 
+WorldCities.prototype.description = function() {
+  return this.landmark + " " + this.tagline;
+}
 
-let destinations = new Destinations();
-let dubai = new WorldCities("Burj Khalifa", "2015", "Make it Happen");
-let jacmel = new WorldCities("Bassin Blue", "2016", "City of light");
-let paris = new WorldCities("Eiffel Tower", "2017", "Fluctuat nec mergitur")
-destinations.addCity(dubai);
-destinations.addCity(jacmel);
-destinations.addCity(paris);
+// let destinations = new Destinations(); 
+// let dubai = new WorldCities("Burj Khalifa", "2015", "Make it Happen");
+// let jacmel = new WorldCities("Bassin Blue", "2016", "City of light");
+// let paris = new WorldCities("Eiffel Tower", "2017", "Fluctuat nec mergitur")
+// destinations.addCity(dubai);
+// destinations.addCity(jacmel);
+// destinations.addCity(paris);
+// let destinations = new Destinations(); 
+
+// UI Logic
+$(document).ready(function() {
+ let destinations = new Destinations(); 
+$("h2").click(function() {
+  let jacmel = new WorldCities("Bassin Blue", "2016", "City of light");
+   
+ console.log(destinations.addCity(jacmel))
+})
+})
+
+
--- readme.md
+++ readme.md
@@ -2,26 +2,32 @@
 
 #### _Webpage that uses constructors to categorize places I've been, 24 August 2020_
 
-#### By _**Cameron Will, Johnny Duverseau**_
+#### By _**Cameron Will <cellardoorsolar@aol.com>, Johnny Duverseau <duverseaujohnny21@gmail.com>**_
 
 ## Description
 
-_This is a simple webpage project to practice using constructors and prototypes. _
+_This is a simple webpage project to practice using constructors and prototypes._
 
 ## Specs for Program Behaivors
-1. For words beginning with a vowel, add "way" to the end.
-Input:
-Output"
-2. 
-3.
-4.
+1. _When user clicks destination, results will populate information from constructor and display them._ 
+### Input: Jacmel
+### Output: photo of bassin-blue, 2016, "City of Light".
+
+2. **_When user clicks destination, results will populate information from constructor and display them.**
+ ### Input: Dubai
+### Output: photo of Burj Khalifa, 2015, "Make it Happen".
+
+3. _When user clicks destination, results will populate information from constructor and display them._
+### Input: Paris
+### Output: photo of Eiffel Tower, 2017, "Fluctuat nec mergitur".
+
 
 ## Setup/Installation Requirements
 
 * _open terminal_
 * _type "cd desktop", then press enter_
-* _type "git clone https://github.com/{username}/{repo}", then press enter_
-* _locate and open folder on desktop named "{name}"_
+* _type "git clone https://github.com/solardor/places", then press enter_
+* _locate and open folder on desktop named "places"_
 * _open the file named "index.html"_
 
 ## Known Bugs
